7ceaf4cd2a003b28ea6afd17c55bcf99d2a0cb16
Anders Pollack <pollack.anders@gmail.com>
2023-02-16 17:37:47 -0800

    Implement `SequenceFactory`

---
+++ _plugins/figures/sequence/factory.js
@@ -0,0 +1,38 @@
+const SequenceItem = require('./index')
+
+/**
+ * The SequenceFactory creates a sequence for a Figure instance
+ */
+module.exports = class SequenceFactory {
+  /**
+   * @param  {Figure} figure
+   */
+  constructor(figure) {
+    this.figure = figure
+  }
+
+  /**
+   * Iterates over a Figure's sequence files and creates annotation sets
+   * with Annotation instances for each item in set.items
+   * @return {<Array[AnnotationSet]>}
+   */
+  create() {
+    const { sequenceFiles } = this.figure
+    if (!sequenceFiles) return
+    return sequenceFiles.map((sequenceItemFilename) => this.sequenceItem(sequenceItemFilename))
+  }
+
+  sequenceItem(sequenceItemFilename) {
+    const { label } = this.figure.data
+    const src = path.join(this.figure.sequenceDir, sequenceItemFilename)
+    console.warn(
+      'SEQUENCE ITEMS IN THE FACTORY',
+      {
+        items: [new SequenceItem(this.figure, { label, src })]
+      }
+    )
+    return {
+      items: [new SequenceItem(this.figure, { label, src })]
+    }
+  }
+}
