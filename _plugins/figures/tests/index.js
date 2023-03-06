2f1c69cc9c0cb368d77eff97ab5b0dcbbf6c1d95
Anders Pollack <pollack.anders@gmail.com>
2023-03-06 15:57:41 -0800

    Add all manifest fixtures, update fixtures directory structure

--- _plugins/figures/tests/index.js
+++ _plugins/figures/tests/index.js
@@ -1,24 +1,20 @@
 require('module-alias/register')
-const test = require('node:test')
+const { mock, test } = require('node:test')
 const assert = require('assert/strict')
 // const { add, subtract, multiply, divide, power } = require('./index')
 const figuresData = require('./__fixtures__/figures/index.js')
 const iiifConfig = require('./__fixtures__/iiifConfig.json')
+const manifest = require('./__fixtures__/manifest.json')
 
 const Figure = require('../figure')
-const processImage
 const figures = Object
   .keys(figuresData)
   .map((figureData) => {
-    return new Figure(iiifConfig, processImage, figureData)
+    return new Figure(iiifConfig, null, figureData)
   })
-// const figure = new Figure(iiifConfig)
 
-// - mock `processImage`
 // https://presentation-validator.iiif.io/validate?version=2.1&url=manifest-url-here
-// create new figure factory?
 
-test('synchronous passing test', (t) => {
-  assert.strictEqual(1, figures)
-})
-
+// test('synchronous passing test', (t) => {
+//   assert.strictEqual(1, 1)
+// })
